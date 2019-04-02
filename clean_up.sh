f3891e2e1d6469d3f94a6bc11b08a41322569b31
Drew Sullivan <dsullivan@allegion.com>
2019-04-02 15:08:39 -0600

    WIP getting runtime down for cleaning up

--- clean_up.sh
+++ clean_up.sh
@@ -13,14 +13,67 @@ delete_sym_links () {
 	    then
             for FILE in ~/*;
 		    do
-                if [ ! -d "$DOT_FILE" ] && [ $(basename $DOT_FILE) = $(basename $FILE) ];
+                if [ -h "$FILE" ] && [ $(basename $DOT_FILE) = $(basename $FILE) ];
                 then
-                    rm "$FILE"
-                    echo deleted "$FILE"
+                    echo "$FILE"
+                    # rm "$FILE"
+                    # echo deleted "$FILE"
                 fi
 		    done
 	    fi
 	done
 }
 
+# WIP
+dsl () {
+    SYM_LINKS=$(find $HOME -maxdepth 1 -type l -ls | sort -n)
+    NUM_SYM_LINKS=$(find $HOME -maxdepth 1 -type l -ls | wc -l)
+    DOT_FILES=$(find $HOME/dot_files/ -maxdepth 1 -name ".*" -type f | sort -n)
+    NUM_DOT_FILES=$(find $HOME/dot_files/ -maxdepth 1 -name ".*" -type f | wc -l)
+
+    declare SYM_LINKS_ARR
+    declare DOT_FILES_ARR
+
+    i=0
+    j=0
+
+    # for item in "$SYM_LINKS";
+    # do
+    #     SYM_LINKS_ARR+=("$item")
+    # done
+
+    # echo ${SYM_LINKS_ARR[@]}
+
+    echo "$i"
+    echo "$NUM_SYM_LINKS"
+
+    while (("$i" < "$NUM_SYM_LINKS")) && (("$j" < "$NUM_DOT_FILES"));
+    do
+        echo "$i"
+        echo true
+        # echo ${SYM_LINKS[$i]}
+        ((i++))
+        # echo ${DOT_FILES[j]}
+        # j++
+        # if [ ${SYM_LINKS[i]} ]
+    done
+
+    # for DOT_FILE in "$DOT_FILES";
+    # do
+    #     echo "$DOT_FILE"
+    # done
+
+    # for SYM_LINK in "$SYM_LINKS";
+    # do
+    #     echo "$SYM_LINK"
+    # done
+
+    # for i in $(seq 1 $NUM_SYM_LINKS);
+    # do
+    #     echo "$i"
+    # done
+
+}
+
 delete_sym_links
+# dsl
