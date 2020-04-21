aa009b972710ac5794caa807fc6016aecfbd7cf2
Drew Sullivan <drew.sullivan.dma@gmail.com>
2020-04-21 13:11:16 -0600

    Small fixes and updates

---
+++ save_everything.sh
@@ -0,0 +1,12 @@
+save_xcode_codesnippets () {
+    rm -rf ~/dot_files/xcode_code_snippets/
+    printf "\nDeleted ~/dot_files/xcode_code_snippets/\n"
+    cp -R ~/Library/Developer/Xcode/UserData/CodeSnippets ~/dot_files/xcode_code_snippets
+    printf "\nCopied all library code snippets to dot_files\n"
+}
+
+save_xcode_codesnippets
+python update_code_snippet_table_in_README.py
+save_dots -e
+
+printf "\nAll utilites have been saved!\n"
